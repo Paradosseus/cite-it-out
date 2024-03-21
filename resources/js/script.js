692827c9352e7b08dba6864664f388b73582ca83
Francis Joshua Collado <colladofrancis524@gmail.com>
2024-03-21 18:01:08 +0800

    Fixed designs

--- resources/js/script.js
+++ resources/js/script.js
@@ -17,8 +17,8 @@ function changePage(page) {
     case "start":
       content.innerHTML = `
       <div class="items-container">
-        <h1>Get Ready</h1>
-        <p id="countdownValue">3</p>
+        <h1 id="ready-message">Get Ready</h1>
+        <p id="countdownValue"></p>
         <a href="index.html"> <button class="button">Back</button></a>
       </div>
         `;
@@ -35,13 +35,15 @@ function changePage(page) {
     case "timeOutPage":
       content.innerHTML = `
       <div class="items-container">
-        <h1>Time's Up!</h1>
-        <h1>Play again?</h1>
+        <h1 class="times-up-message">Time's Up!</h1>
         <audio id="times-up-audio" autoplay>
         <source src="resources/media/times-up.mp3" type="audio/mpeg">
         </audio>
+        <div>
+        <h1 class="play-again-message">Play again?</h1>
         <button class="button" onclick="changePage('start')">Yes</button>
         <button class="button" onclick="changePage('main')">No</button>
+        </div>
         </div>
         `;
       break;
@@ -62,6 +64,7 @@ function countDown() {
     if (value < 0) {
       clearInterval(startCountdown);
       document.getElementById("countdownValue").innerHTML = "";
+      document.getElementById("ready-message").classList.add("hide");
       generateCategory();
     }
   }, 1000);
@@ -72,7 +75,7 @@ function generateCategory() {
     categories[Math.floor(Math.random() * categories.length) + 1];
 
   const category = document.createElement("p");
-  category.id = "categoryValue";
+  category.id = "category-value";
   category.innerHTML = selectedCategory;
   const counterElement = document.getElementById("countdownValue");
 
@@ -95,8 +98,3 @@ function startTimer() {
 }
 
 const timeUpAudio = document.getElementById("times-up-audio").autoplay;
-
-// timeUpAudio.addEventListener("canplaythrough", function (event) {
-//   timeUpAudio.autoplay = true;
-//   timeUpAudio.load();
-// });
